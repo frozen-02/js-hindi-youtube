46de93b6eaa119f9dae3c3796e17a1a32450fcc3
frozen-02 <144924316+frozen-02@users.noreply.github.com>
2025-07-20 06:20:54 +0000

    Conversion Of DataTypes

---
+++ 01_basics/03_ConversionOperations.js
@@ -0,0 +1,35 @@
+let score = 33
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)         // Conversion line // Number = Conversion Operator
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN ( Not A Number)
+// "Omkar" => NaN
+// true => 1
+// false => 0
+// null = 0
+// undefined = NaN
+
+let loggedIn = "Omkar"
+
+let booleanLogedIn = Boolean(loggedIn)  // Conversion line // Boolean = Conversion Operator
+//console.log(typeof loggedIn);
+//console.log(loggedIn);
+//console.log(booleanLogedIn);
+
+// 1 => true ; 0 => false
+// "" => false
+// "Omkar" => true
+
+let somenumber = 543
+
+let stringNumber = String(somenumber)
+console.log(somenumber);
+console.log(typeof stringNumber);
+
+
