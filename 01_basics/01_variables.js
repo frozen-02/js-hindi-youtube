a93a43979b5b510a342211029c273c068f41e4b0
frozen-02 <144924316+frozen-02@users.noreply.github.com>
2025-07-20 05:54:41 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 1521612
+var accountEmail = "ghaskih@google.com"
+let accountPassword = "61264"
+accountCity = "ugsioh"
+
+console.table([accountId , accountEmail , accountPassword , accountCity])
---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,22 @@
+"use strict" // treat all the JS code as newer version
+
+console.log("omkar");
+
+
+// alert(3+3) //we are using node.js not browser
+
+// Data Types
+// Number = 2^53
+// bigint 
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined 
+// symbol = unique
+
+// object
+
+console.log(typeof "omkar")
+
+console.log(typeof undefined) // undefined
+console.log(typeof null)      // object
